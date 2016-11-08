286944900f79a3adf60a334976a5c4927210cb82
Brian Love <brian@brianflove.com>
2016-11-08 16:03:14 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# TypeScript 2 + Express + Node.js
+
+This is a repository to go with my article on creating an Express web application using TypeScript 2.
+
+## Install
+
+Install the node packages via:
+
+`$ npm install`
+
+And then run the grunt task to compile the TypeScript:
+
+`$ npm run grunt`
+
+## Starting
+
+To start the server run:
+
+`$ npm start`
---
+++ src/server.ts
@@ -0,0 +1,121 @@
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as express from "express";
+import * as logger from "morgan";
+import * as path from "path";
+import errorHandler = require("errorhandler");
+import methodOverride = require("method-override");
+
+import { IndexRoute } from "./routes/index";
+
+/**
+ * The server.
+ *
+ * @class Server
+ */
+export class Server {
+
+  public app: express.Application;
+
+  /**
+   * Bootstrap the application.
+   *
+   * @class Server
+   * @method bootstrap
+   * @static
+   * @return {ng.auto.IInjectorService} Returns the newly created injector for this app.
+   */
+  public static bootstrap(): Server {
+    return new Server();
+  }
+
+  /**
+   * Constructor.
+   *
+   * @class Server
+   * @constructor
+   */
+  constructor() {
+    //create expressjs application
+    this.app = express();
+
+    //configure application
+    this.config();
+
+    //add routes
+    this.routes();
+
+    //add api
+    this.api();
+  }
+
+  /**
+   * Create REST API routes
+   *
+   * @class Server
+   * @method api
+   */
+  public api() {
+    //empty for now
+  }
+
+  /**
+   * Configure application
+   *
+   * @class Server
+   * @method config
+   */
+  public config() {
+    //add static paths
+    this.app.use(express.static(path.join(__dirname, "public")));
+
+    //configure pug
+    this.app.set("views", path.join(__dirname, "views"));
+    this.app.set("view engine", "pug");
+
+    //mount logger
+    this.app.use(logger("dev"));
+
+    //mount json form parser
+    this.app.use(bodyParser.json());
+
+    //mount query string parser
+    this.app.use(bodyParser.urlencoded({
+      extended: true
+    }));
+
+    //mount cookie parker
+    this.app.use(cookieParser("SECRET_GOES_HERE"));
+
+    //mount override?
+    this.app.use(methodOverride());
+
+    // catch 404 and forward to error handler
+    this.app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+        err.status = 404;
+        next(err);
+    });
+
+    //error handling
+    this.app.use(errorHandler());
+  }
+
+  /**
+   * Create and return Router.
+   *
+   * @class Server
+   * @method config
+   * @return void
+   */
+  private routes() {
+    let router: express.Router;
+    router = express.Router();
+
+    //IndexRoute
+    IndexRoute.create(router);
+
+    //use router middleware
+    this.app.use(router);
+  }
+
+}
